dfea4065c749b41e53a89bce595d42b51add8b10
sumit <sumitmatte29@gmail.com>
2023-08-22 16:09:09 +0530

    Added Two Repository

---
+++ simple-login/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [
+    // other imports
+    FormsModule
+  ],
+  // other declarations, providers, etc.
+})
+export class AppModule { }
+
